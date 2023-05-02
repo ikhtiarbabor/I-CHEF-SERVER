d250852d5ba9bad198cd53226279315fac6b88a7
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2023-05-02 09:56:39 +0600

    git commit -mcld

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 2000;
 const cors = require('cors');
+const allChef = require('./data/allChef.json');
+const food = require('./data/Food.json');
 app.use(cors());
 
-
 app.get('/', (req, res) => {
-  res.send('assignment 10 is starting');
+  res.send(allChef);
 });
 app.listen(port);
