f2b892cc53420722a4ff26adcf473a76726af67c
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2023-05-02 17:14:31 +0600

    recipies and a chef data send

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const app = express();
 const port = process.env.PORT || 2000;
 const cors = require('cors');
 const allChef = require('./data/allChef.json');
-const food = require('./data/Food.json');
+const recipes = require('./data/Food.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send(allChef);
+});
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  const chef = allChef.find((chef) => chef.id === id);
+  res.send(chef);
+});
+app.get('/recipes/:chef_code', (req, res) => {
+  const chef_code = req.params.chef_code;
+  const chef = allChef.find((chef) => chef.id === chef_code);
+  console.log(chef);
+  const chefRecipes = recipes.filter(
+    (recipe) => recipe.chef_code === chef_code
+  );
+  const chefAndFood = [chef, ...chefRecipes];
+  res.send(chefAndFood);
 });
 app.listen(port);
